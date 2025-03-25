4c13df4d0c432c9fc27a827f79e2e075d30dfd74
JH1476 <junghun14760@naver.com>
2025-03-26 00:02:29 +0900

    Create 꼬마 정민.java

---
+++ BOJ/250325/11382/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        long C = scanner.nextLong();
+
+        System.out.println(A+B+C);
+
+    }
+}
