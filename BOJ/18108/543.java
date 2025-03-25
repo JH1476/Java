f18e2e631ae0e2e5349f1dfcec7355e8470f5a17
JH1476 <junghun14760@naver.com>
2025-03-25 22:37:57 +0900

    Create 543.java

---
+++ BOJ/18108/543.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+            System.out.println(year-543);
+    }
+}
