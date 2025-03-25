ab04e623db3d0939e92b1cf7f584f95934e8b057
JH1476 <junghun14760@naver.com>
2025-03-25 21:49:32 +0900

    Create AB.java

---
+++ BOJ/1008/AB.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if(a > 0 && b < 10) {
+        System.out.println((double)a/b);
+        }
+    }
+}
