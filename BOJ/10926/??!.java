1814ae8857eacd473b064efa227d8d03cdeb9253
JH1476 <junghun14760@naver.com>
2025-03-25 22:23:24 +0900

    Create ??!.java

---
+++ BOJ/10926/??!.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+            System.out.println(a + "??!");
+    }
+}
