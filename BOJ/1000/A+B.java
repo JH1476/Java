a6688a63b086d433463f573da08a8074e9eaeed6
JH1476 <junghun14760@naver.com>
2025-03-25 13:56:31 +0900

    Create A+B.java

---
+++ BOJ/1000/A+B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+b);
+    }
+}
