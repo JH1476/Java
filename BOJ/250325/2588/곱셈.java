cdafbdcce1657d8035d30b3c7967a4895f42bd65
JH1476 <junghun14760@naver.com>
2025-03-25 23:49:30 +0900

    Update 곱셈.java

--- BOJ/250325/2588/곱셈.java
+++ BOJ/250325/2588/곱셈.java
@@ -5,12 +5,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         int A = scanner.nextInt();
-        scanner.nextLine();
-        String B = scanner.nextLine();
-        System.out.println(A*(B.charAt(2)-'0'));
-        System.out.println(A*(B.charAt(1)-'0'));
-        System.out.println(A*(B.charAt(0)-'0'));
-        System.out.println(A*Integer.parseInt(B));
+        int B = scanner.nextInt();
+
+        int num1 = B%10;
+        int num2 = B/10%10;
+        int num3 = B/100;
+
+        System.out.println(A*num1);
+        System.out.println(A*num2);
+        System.out.println(A*num3);
+        System.out.println(A*B);
 
     }
 }
