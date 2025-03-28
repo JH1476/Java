3c48b94d084032b930bcfd12aeed90f33501ce52
JH1476 <junghun14760@naver.com>
2025-03-29 01:32:21 +0900

    Create A+B-7.java

---
+++ BOJ/250329/11021/A+B-7.java
@@ -0,0 +1,26 @@
+    import java.util.*;
+    import java.io.*;
+
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            int T = Integer.parseInt(br.readLine());
+            int[] sum = new int[T];
+
+            for(int i = 0; i < T; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+                sum[i] = A + B;
+            }
+            for(int j = 0; j < T; j++) {
+                bw.write("Case #" + (j+1) + ": " + sum[j] + " ");
+                bw.write("\n");
+            }
+            bw.flush();
+            bw.close();
+
+        }
+    }
